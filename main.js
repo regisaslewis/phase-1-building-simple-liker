8bb6233e9ee50c0ab0da801f556e75f11bf762bc
RegisLewis <regisaslewis@gmail.com>
2023-02-13 22:34:10 -0800

    building the simple liker app

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartIconList = document.querySelectorAll(".like-glyph");
+const modal = document.querySelector("#modal");
 
+function heartClick() {
+  heartIconList.forEach(e => {
+    e.addEventListener("click", () => {
+      if (e.textContent === EMPTY_HEART) {
+          mimicServerCall()
+          .then(() => {
+            e.textContent = FULL_HEART;
+            e.setAttribute("class", "activated-heart");
+          })
+          .catch((error) => {
+            modal.removeAttribute("class");
+            modal.textContent = error;
+            setTimeout(() => {
+              modal.setAttribute("class", "hidden");
+            }, 3000)
+          })
+      } else {
+        e.textContent = EMPTY_HEART;
+        e.removeAttribute("class");
+      };
+    })
+  });
+}
 
+document.addEventListener("DOMContentLoaded", heartClick);
 
 
 //------------------------------------------------------------------------------
